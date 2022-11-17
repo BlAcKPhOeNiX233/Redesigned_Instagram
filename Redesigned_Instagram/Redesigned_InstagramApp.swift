ca9e66285a75eb02f7aa9333ec19b102883780b2
BlAcKPhOeNiX233 <bryansanchez.311@gmail.com>
2022-11-17 11:51:32 +0100

    Initial Commit

---
+++ Redesigned_Instagram/Redesigned_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redesigned_InstagramApp.swift
+//  Redesigned_Instagram
+//
+//  Created by Bryan Sánchez Peralta on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Redesigned_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
